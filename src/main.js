7129093ab8e2b1cbcd1941cfded00b887e90de94
Chris Chen <chiandeye@gmail.com>
2021-11-21 04:32:39 +0800

    Add setting, vee-validate

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import { createApp } from 'vue'
+import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate'
+import { required, email, min } from '@vee-validate/rules'
+import { localize, setLocale } from '@vee-validate/i18n'
+import en from '@vee-validate/i18n/dist/locale/en.json'
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
+
+import App from './App.vue'
+import router from './router'
+
+defineRule('required', required)
+defineRule('email', email)
+defineRule('min', min)
+configure({
+  generateMessage: localize({ en: en, zh_TW: zhTW }),
+  validateOnInput: true
+
+})
+setLocale('en')
+
+configure({
+  generateMessage: localize({
+    en,
+    zhTW
+  })
+})
+
+const app = createApp(App).use(router)
+
+app.component('Form', Form)
+app._component('Field', Field)
+app.component('ErrorMessage', ErrorMessage)
+
+app.mount('#app')
